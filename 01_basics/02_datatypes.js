10ce8a26f8fd3248cd2d03ccdbb370366f18a8d6
Reenu09 <reenubishnoi2005@gmail.com>
2025-07-11 08:55:57 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,32 @@
+// documentations to refer for javascript are mdn , Ecma
+
+"use strict"; //treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3
+    +
+    3
+) //code readability should be high
+
+console.log("Reenu")
+
+let name = "Reenu"
+let age = 20
+let isLoggedIn = false
+
+// Data types :
+// number = 2 to power 53
+// bigint
+//string = ""
+// boolean
+// null = standalone value
+// undefined 
+//symbol = for uniqueness
+
+// object
+console.log(typeof "reenu"); //string
+console.log(typeof age); // number
+console.log(typeof null); //object
+console.log(typeof undefined); // undefined
+
