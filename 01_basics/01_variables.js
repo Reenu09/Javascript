e2ee7dd358d5823e515269bc779e5577472278a1
Reenu09 <reenubishnoi2005@gmail.com>
2025-07-11 08:56:42 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountID = 144553
+let accountEmail = "reenu@google.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+//accountId=2
+// var is not used generally because of the issue in block scope and functional scope
+accountEmail = "reb@google.com"
+accountPassword = "1112121"
+accountCity = "bengaluru"
+let accountState;
+
+console.table([accountID,accountEmail,accountPassword,accountCity,accountState]);
