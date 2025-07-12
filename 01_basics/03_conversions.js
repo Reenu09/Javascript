fe8b6e2a102913826eb26cfcc35293b741d8e60e
Reenu09 <reenubishnoi2005@gmail.com>
2025-07-12 07:58:19 +0000

    Conversions of data types

---
+++ 01_basics/03_conversions.js
@@ -0,0 +1,33 @@
+//let score = '33abc'
+//console.log(typeof score); //string
+//let valueInNumber = Number(score) // conversion from string to number
+//console.log(typeof valueInNumber); // number
+//console.log(valueInNumber); // NaN
+// it means we have converted the string '33abc' into number using Number()
+// but when we checked the typeof that number which is stored in valueInNumber , it is NaN
+// NaN is not a number but still its type is number
+
+//let score = null
+//console.log(typeof null); //object
+//let score = undefined
+//console.log(score); //undefined
+
+//let isLoggedIn = 1
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn); //true
+//console.log(typeof booleanIsLoggedIn); //boolean
+
+//let isLoggedIn = ""
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn); // false
+
+
+//let isLoggedIn = "Reenu"
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn); //true
+
+let anynumber = 33
+let stringNumber = String(anynumber)
+console.log(stringNumber); //33
+console.log(typeof stringNumber); //string
+
